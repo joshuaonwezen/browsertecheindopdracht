ad5c657b994fadfa142414c380b09788e5789f71
Joshua Onwezen <joshua_onwezen@hotmail.com>
2016-05-18 14:13:20 +0200

    Fixed issue 2

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,49 +1,49 @@
-(function () {
-    var dragDrop = {
-        item: null,
-        itemId: null,
-        itemClick: function (item) {
-
-            item.addEventListener('click', function () {
-                if (dragDrop.item == null) {
-                    item.parentNode.removeChild(item);
-                    dragDrop.item = '<img src="'+item.src+'" draggable="true" ondragstart="drag(event)" class="dragitem" id="'+item.id+'" width="328" height="305">';
-                    dragDrop.itemId = item.id;
-                }
-            });
-
-        },
-        
-        frameClick: function(frame){
-            
-            frame.addEventListener('click', function(event){
-                if (frame !== event.target) {
-                    return;
-                }
-                if(dragDrop.item != null){
-                    frame.innerHTML += dragDrop.item;
-                    dragDrop.itemClick(document.getElementById(dragDrop.itemId));
-                    dragDrop.item = null;
-                    dragDrop.itemId = null;
-                }
-            }, false);
-        }
-        
-    }
-
-    var items = document.querySelectorAll(".dragitem")
-
-    for(i = 0; i < items.length; i++){
-        dragDrop.itemClick(items[i]);
-    }
-    
-    var frames = document.querySelectorAll("section");    
-    
-    for(i = 0; i < frames.length; i++){
-        dragDrop.frameClick(frames[i]);
-    }
-    
-})();
+//(function () {
+//    var dragDrop = {
+//        item: null,
+//        itemId: null,
+//        itemClick: function (item) {
+//
+//            item.addEventListener('click', function () {
+//                if (dragDrop.item == null) {
+//                    item.parentNode.removeChild(item);
+//                    dragDrop.item = '<img src="'+item.src+'" draggable="true" ondragstart="drag(event)" class="dragitem" id="'+item.id+'" width="328" height="305">';
+//                    dragDrop.itemId = item.id;
+//                }
+//            });
+//
+//        },
+//        
+//        frameClick: function(frame){
+//            
+//            frame.addEventListener('click', function(event){
+//                if (frame !== event.target) {
+//                    return;
+//                }
+//                if(dragDrop.item != null){
+//                    frame.innerHTML += dragDrop.item;
+//                    dragDrop.itemClick(document.getElementById(dragDrop.itemId));
+//                    dragDrop.item = null;
+//                    dragDrop.itemId = null;
+//                }
+//            }, false);
+//        }
+//        
+//    }
+//
+//    var items = document.querySelectorAll(".dragitem")
+//
+//    for(i = 0; i < items.length; i++){
+//        dragDrop.itemClick(items[i]);
+//    }
+//    
+//    var frames = document.querySelectorAll("section");    
+//    
+//    for(i = 0; i < frames.length; i++){
+//        dragDrop.frameClick(frames[i]);
+//    }
+//    
+//})();
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -52,14 +52,14 @@ function allowDrop(ev) {
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
     var parent = document.getElementById(ev.target.id).parentNode;
-    parent.classList.add('selected');
     
-    console.log(parent);    
     if (parent.id == 'right') {
-        document.getElementById('left').classList.add('target');
+        parent.className = 'right-frame frame selected';
+        document.getElementById('left').className = 'left-frame frame target';
     }
     if (parent.id == 'left') {
-        document.getElementById('right').classList.add('target');
+        parent.className = 'left-frame frame selected';
+        document.getElementById('right').className = 'right-frame frame target';
     }
 }
 
@@ -69,13 +69,14 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
     
     var parent = document.getElementById(ev.target.id);
-    parent.classList.remove('target');
 
     if(parent.id == 'right'){
-        document.getElementById('left').classList.remove('selected');
+        parent.className = 'right frame frame';
+        document.getElementById('left').className = 'left-frame frame';
     }
     if(parent.id == 'left'){
-        document.getElementById('right').classList.remove('selected');
+        parent.className = 'left-frame frame';
+        document.getElementById('right').className = 'right-frame frame';
     }
 }
 
